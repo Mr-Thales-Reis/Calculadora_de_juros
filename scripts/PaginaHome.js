ba675efc8fa21cc7b80bb633a15f42dbe2cc086c
Thales <thalesreis.es@gmail.com>
2025-09-29 11:37:33 -0300

    Finalizando a página home

---
+++ scripts/PaginaHome.js
@@ -0,0 +1,14 @@
+const inputNomeUsuario = document.getElementById("nome");
+const buttonSalvar = document.getElementById("button");
+const mensagem = document.getElementById("mensagem");
+
+buttonSalvar.addEventListener("click", evento => {
+    evento.preventDefault();
+
+    if(inputNomeUsuario.value === ""){
+        window.alert("Ops, você se esqueceu de preencher o campo nome!!")
+    }
+
+    mensagem.textContent = `Olá, ${inputNomeUsuario.value}, seja bem-vindo! 👑`
+
+})
